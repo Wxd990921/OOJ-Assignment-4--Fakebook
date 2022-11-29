968b2cb4f094b6f426b848491854038d2ce6a39d
Xidi Wang <wangxidi0921@gmail.com>
2022-11-29 14:46:13 -0600

    Final Vision

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,39 @@
 'use strict';
-let message = document.getElementById('message');
-let btn = document.getElementById('btn');
-let output = document.getElementById('output');
-let tp = document.getElementById('tp');
-
-function textIn() {
-	if (message.value == '') {
-		alert('text cannot be empty');
-		return false;
-	}
-	createM();
-	message.value = '';
-}
-
-function createM() {
-	let newDiv = document.createElement('div');
-	newDiv.className = 'comment';
-	output.appendChild(newDiv);
-	console.log(message.value);
-	let sentences = message.value;
-	let li = document.createElement('div');
-	li.innerHTML = sentences;
-	console.log(li);
-	newDiv.appendChild(li);
-	let deleMsg = document.createElement('a');
-	deleMsg.href = '#'
-	deleMsg.className = 'delete'
-	deleMsg.innerHTML = 'Delete Message'
-	newDiv.appendChild(deleMsg);
-	deleMsg.onclick = function () {
-		let conf = confirm('Confirm delete.');
-		if (conf) {
-			output.removeChild(newDiv);
+window.onload = function () {
+	let commBox = document.querySelector("#commBox");
+	let ul = document.querySelector("ul");
+	let combtn = document.querySelector(".combtn");
+	combtn.addEventListener('click', function () {
+		if (!commBox.value) {
+			console.log("Comments are empty");
+		} else {
+			let li = document.createElement("li");
+			li.innerHTML = 
+			"<img src='./assets/img/82501d3b-e89d-43a2-8b07-09751494467d.png'>" + "<span>Darren</span> <span class='time'></span>" 
+			+ "<p>" + commBox.value + "</p>" + "<a href='javascript:;'>Delete</a>";
+			ul.insertBefore(li, ul.children[0]);
+			getNowTime();
+			commBox.value = '';
+			let del = document.querySelectorAll("a");
+			for (let i = 0; i < del.length; i++) {
+				del[i].onclick = function () {
+					ul.removeChild(this.parentNode);
+				}
+			}
 		}
+	})
 
+	function getNowTime() {
+		let date = new Date();
+		let year = date.getFullYear(); 
+		let month = date.getMonth() + 1;
+		let day = date.getDate();
+		let commTime = document.querySelector(".time")
+		let time = year + '-' + addZero(month) + '-' + addZero(day);
+		commTime.innerHTML = time;
+	}
+
+	function addZero(s) {
+		return s < 10 ? ('0' + s) : s;
 	}
 }
-btn.onclick = function () {
-	createM();
-}
-
